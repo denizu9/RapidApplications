607f4ed3ec2a71870e4a7c13f2a515895bcd51e8
denizu9 <denizural9@gmail.com>
2024-03-12 23:49:53 +0300

    Updates

--- CampIntro/Program.cs
+++ CampIntro/Program.cs
@@ -3,6 +3,20 @@
     private static void Main(string[] args)
     {
         string categoryTag = "Category";
+
+        double dolarPriceToday = 32.05;
+
+        double dolarPriceYesterday = 31.84;
+
+        if (dolarPriceToday > dolarPriceYesterday)
+        {
+            Console.WriteLine("Dolar Incresead!");
+        }
+        else
+        {
+            Console.WriteLine("Dolar Decreased!");
+        }
+
         Console.WriteLine(categoryTag);
     }
 }
