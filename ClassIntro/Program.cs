840db3fd77b6d17b3a2c5aeb94ea5e8de9e6e5c6
denizu9 <denizural9@gmail.com>
2024-03-13 00:38:37 +0300

    Class Intro Added

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Course course1 = new Course();
+        course1.courseName = "C#";
+        course1.instructor = "Engin Demiroğ";
+        course1.watchCount = 3200;
+
+        Console.WriteLine("Course Name : " + " " + course1.courseName + ", " + "Instructor : " + " " + course1.instructor 
+            + ", " + "Watch Count : " + " " + course1.watchCount);
+    }
+
+    class Course
+    {
+        public string courseName { get; set; }
+        public string instructor { get; set; }
+        public int watchCount { get; set; }
+    }
+}
